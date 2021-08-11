df597230d7de276b17b808af724a9533ff9c9e34
Assis de Lima Silva <assis.silva@vectracs.com.br>
2021-08-10 23:00:44 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { FuncionarioComponent } from './funcionario/funcionario.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'produtos', component: ProdutoComponent },
+  { path: 'funcionarios', component: FuncionarioComponent },
+  { path: '',   redirectTo: '/produtos', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+
+]; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    FuncionarioComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
